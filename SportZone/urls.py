52f8587d1d02b68f9730a82650676185f6782de9
unknown <joao.miranda20018@gmail.com>
2024-06-26 20:54:09 -0300

    pagina inicial

--- SportZone/urls.py
+++ SportZone/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("polls/", include('polls.urls')),
+    path("", include('polls.urls')),
     path("admin/", admin.site.urls),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from polls.models import Produto
 
 def index(request):
-  return HttpResponse("SportZone")
+    produtos = Produto.objects.order_by('nome')
+    context = {'produtos': produtos}
+    return render(request, 'index.html', context)
+
