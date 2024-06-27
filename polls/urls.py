0443bc4b68db232f12932e3f749f95239b758af1
unknown <joao.miranda20018@gmail.com>
2024-06-27 18:01:49 -0300

    Produtos

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),
+    path('produto/<int:produto_id>/', views.produto_detail, name='produto_detail'),
+    path('carrinho_compra/', views.carrinho_compra, name='carrinho_compra'),  # Defina a URL para carrinho_compra
+
+    # Outras URLs do seu aplicativo
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from polls.models import Produto
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Produto, Categoria, Compra, CompraProdutoItem
 
 def index(request):
-    produtos = Produto.objects.order_by('nome')
-    context = {'produtos': produtos}
+    categorias = Categoria.objects.all()
+    context = {
+        'categorias': categorias,
+    }
     return render(request, 'index.html', context)
+
+def produto_detail(request, produto_id):
+    produto = get_object_or_404(Produto, pk=produto_id)
+    context = {
+        'produto': produto,
+    }
+    return render(request, 'produto_detail.html', context)
+
+def carrinho_compra(request):
+    if request.method == 'POST':
+        produto_id = request.POST.get('produto_id')
+        produto = get_object_or_404(Produto, pk=produto_id)
+        return HttpResponseRedirect(reverse('index'))
+    return render(request, 'carrinho_compra.html')
+
+def adicionar_ao_carrinho(request):
+    if request.method == 'POST':
+        produto_id = request.POST.get('produto_id')
+        quantidade = int(request.POST.get('quantidade', 1)) 
+        produto = get_object_or_404(Produto, pk=produto_id)
+        compra, _ = Compra.objects.get_or_create(cliente=request.user, status='em andamento')
+      
+        item_existente = compra.compraprodutoitem_set.filter(produto=produto).first()
+        if item_existente:
+            item_existente.quantidade += quantidade
+            item_existente.save()
+        else:
+            novo_item = CompraProdutoItem(compra=compra, produto=produto, quantidade=quantidade, valor=produto.preco_venda)
+            novo_item.save()
+        
+        return redirect('produto_detail', produto_id=produto_id)
+    return redirect('pagina_principal')  
 
